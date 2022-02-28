722f60958527d529192a98fc9906d85e5b658d21
Mr. Remark <itsmine096@gmail.com>
2022-02-28 15:50:05 +0900

    MemoCell 클래스 생성, 객체 연결

---
+++ Memory_Teller/Memory_Teller/Sources/Cells/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  Memory_Teller
+//
+//  Created by Hyun on 2022/02/28.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
