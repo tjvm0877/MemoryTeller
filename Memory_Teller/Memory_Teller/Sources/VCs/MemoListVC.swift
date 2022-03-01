ada1bbca0efd4f3e06ecf8afad332b5c4ab7b536
Mr. Remark <itsmine096@gmail.com>
2022-03-01 14:56:00 +0900

    MemoListVC 클래스 구현

--- Memory_Teller/Memory_Teller/Sources/VCs/MemoListVC.swift
+++ Memory_Teller/Memory_Teller/Sources/VCs/MemoListVC.swift
@@ -9,16 +9,46 @@ import UIKit
 
 class MemoListVC: UITableViewController {
 
+    // 앱 델리게이트 객체의 참조 정보를 읽어온다.
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.reloadData()
     }
-
+    
+    // 셀의 갯수 결정
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let count = self.appDelegate.memoList.count
+        return count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memoList[indexPath.row]
+        
+        // 이미지 속성이 비어 있을 경우 "memoCell", 아니면 "memoCellWithImage"
+        let cellId = row.image == nil ? "memoCell" : "memoCellWithImage"
+        
+        // 재사용 큐로부터 프로토타입 셀의 인스턴스를 전달받는다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
+        
+        // memoCell의 내용을 구성한다.
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        // Date 타입의 날짜를 yyyy-MM-dd HH:mm:ss 포멧에 맞게 변경
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.regdate?.text = formatter.string(from: row.regDate!)
+        
+        // cell 객체를 리턴
+        return cell
+    }
+    
 }
