7f5ef2a57af798d6f89141a91f77216b09dcca94
Mr. Remark <itsmine096@gmail.com>
2022-03-10 18:57:39 +0900

    사이드 바 이미지 변경, 헤더 변경( 프로필 이미지 추가, 이름 추가, 이메일 변경)

--- Memory_Teller/Memory_Teller/Sources/VCs/SideBarVC.swift
+++ Memory_Teller/Memory_Teller/Sources/VCs/SideBarVC.swift
@@ -8,14 +8,18 @@
 import UIKit
 
 class SideBarVC: UITableViewController {
-
+    let nameLbl = UILabel()
+    let emailLbl = UILabel()
+    let profileImage = UIImageView()
+    
     // 메뉴 제목 배열
     let titles = [
-        "메뉴 01",
-        "메뉴 02",
-        "메뉴 03",
-        "메뉴 04",
-        "메뉴 05"
+        "새글 작성하기",
+        "친구 새글",
+        "달력으로 보기",
+        "공지사항",
+        "통계",
+        "계정 관리"
     ]
     
     //  메뉴 아이콘 배열
@@ -24,29 +28,45 @@ class SideBarVC: UITableViewController {
         UIImage(named: "icon02.png"),
         UIImage(named: "icon03.png"),
         UIImage(named: "icon04.png"),
-        UIImage(named: "icon05.png")
+        UIImage(named: "icon05.png"),
+        UIImage(named: "icon06.png")
     ]
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        // 테이블 뷰의 헤더 역할을 할 뷰를 정의한다.
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
+        headerView.backgroundColor = .brown
         
-        // 계정 정보를 표시할 레이블 객체를 정의
-        let accountLabel = UILabel()
-        accountLabel.frame = CGRect(x: 10, y: 30, width: self.view.frame.width, height: 30)
+        // 이름 레이블 속성 정의, 헤더뷰에 추가
+        self.nameLbl.frame = CGRect(x: 70, y: 15, width: 100, height: 30) // 위치, 크기를 정의
+        self.nameLbl.text = "Hello?" // 기본 텍스트
+        self.nameLbl.textColor = .white // 텍스트 색상
+        self.nameLbl.font = UIFont.boldSystemFont(ofSize: 15) // 폰트 사이즈
+        self.emailLbl.backgroundColor = .clear // 배경 색상
+        headerView.addSubview(self.nameLbl) // 헤더뷰에 추가
         
-        accountLabel.text = "Its mine"
-        accountLabel.textColor = .white
-        accountLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        // 이메일 레이블의 속성을 정의, 헤더 뷰에 추가
+        self.emailLbl.frame = CGRect(x: 70, y: 30, width: 100, height: 30) // 위치와 크기를 정의
+        self.emailLbl.text = "tjvm12@gmail.com" // 기본 텍스트
+        self.emailLbl.textColor = .white // 텍스트 색상
+        self.emailLbl.font = UIFont.systemFont(ofSize: 11) // 폰트 사이즈
+        self.emailLbl.backgroundColor = .clear // 배경 색상
+        headerView.addSubview(self.emailLbl) // 헤더 뷰에 추가
         
-        // 테이블 뷰 상단에 표시될 뷰를 정의
-        let v = UIView()
-        v.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70)
-        v.backgroundColor = .brown
-        v.addSubview(accountLabel)
+        // 기본 이미지를 구현
+        let defaultProfile = UIImage(named: "account.jpg")
+        self.profileImage.image = defaultProfile // 이미지 등록
+        self.profileImage.frame = CGRect(x: 10, y: 10, width: 50, height: 50)
+        view.addSubview(self.profileImage) // 헤더 뷰에 추가
         
-        // 생성된 뷰 v를 테이블 헤더 뷰 영역에 등록
-        self.tableView.tableHeaderView = v
+        // 추가) 프로필 이미지 둥글게 만들기
+        self.profileImage.layer.cornerRadius = (self.profileImage.frame.width / 2) // 반원 형태로 라운딩
+        self.profileImage.layer.borderWidth = 0 // 테두리 두계 0으로
+        self.profileImage.layer.masksToBounds = true // 마스크 효과
+        view.addSubview(self.profileImage)
         
+        // 테이블 뷰의 헤더 뷰로 지정한다.
+        self.tableView.tableHeaderView = headerView
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
